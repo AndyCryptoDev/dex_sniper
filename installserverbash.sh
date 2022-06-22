5f67a0cc5bde614c270820ad9348551f06e8ca9d
Andy <103894785+AndyCryptoDev@users.noreply.github.com>
2022-06-22 15:58:51 +0300

    Add files via upload

---
+++ installserverbash.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+#First, let's update our linux
+
+apt update
+apt list --upgradable
+apt upgrade
+
+#Now let's configure FireWall
+
+ufw allow OpenSSH
+ufw allow 8443
+ufw enable
+ufw status verbose
+
+#Now to use the bot, let's install python and libraries 
+
+apt install python3.8
+apt install python3-pip
+pip3 install pip --upgrade
+pip3 install setuptools --upgrade
+pip3 install web3
+pip3 install pytelegrambotapi
+pip3 install cherrypy
+pip3 install pysqlite3
+pip3 install binance-connector
+apt install unzip
+unzip swagger.zip
+ls -l
+python3 setup.py install --user
+
+#Generation of self-signed SSL certificates
+
+apt-get install openssl
+openssl genrsa -out webhook_pkey.pem 2048
+openssl req -new -x509 -days 3650 -key webhook_pkey.pem -out webhook_cert.pem
+
+ls -l 
+
+
