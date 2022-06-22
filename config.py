75fb100d1f868d3e9054ef9f807e34c5f29aad4f
Andy <103894785+AndyCryptoDev@users.noreply.github.com>
2022-06-22 15:27:52 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+#Insert all values between ' '
+WEBHOOK_HOST = '' #Your server IP address
+WEBHOOK_LISTEN = ''  #Your server IP address
+api_tel_bot  = '' #Telegram bot API key (from @botfather)
+wss = '' #WSS endpoint
+sender_address = '' #your wallet address
+private_key = ''#your private key from your wallet
+swagger_api = '' #from https://moralis.io
