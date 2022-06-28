e39cdfefe85b88579f3d6f3e63cb459e0b5483ef
Andy <103894785+AndyCryptoDev@users.noreply.github.com>
2022-06-28 03:46:52 +0300

    Update config_snp.py

--- config_snp.py
+++ config_snp.py
@@ -2,7 +2,6 @@
 WEBHOOK_HOST = '' #Your server IP address
 WEBHOOK_LISTEN = ''  #Your server IP address
 api_tel_bot  = '' #Telegram bot API key (from @botfather)
-wss = '' #WSS endpoint (https://moralis.io or you can use any convenient)
+wss = '' #WSS endpoint (https://www.quicknode.com/ or you can use any convenient)
 sender_address = '' #your wallet address
-private_key = ''#your private key from your wallet
-swagger_api = '' #from https://moralis.io
+private_key = '' #your private key from your wallet
