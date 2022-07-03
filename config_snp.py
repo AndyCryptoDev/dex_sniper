d2bec6ab56959479f841d2e37b6e149bc764e17d
Andy <103894785+AndyCryptoDev@users.noreply.github.com>
2022-07-04 00:00:45 +0200

    Update config_snp.py

--- config_snp.py
+++ config_snp.py
@@ -2,6 +2,6 @@
 WEBHOOK_HOST = '' #Your server IP address
 WEBHOOK_LISTEN = ''  #Your server IP address
 api_tel_bot  = '' #Telegram bot API key (from @botfather)
-wss = '' #WSS endpoint (https://www.quicknode.com/ or you can use any convenient)
+wss2 = '' #WSS endpoint (https://www.quicknode.com/ or you can use any convenient)
 sender_address = '' #your wallet address
 private_key = '' #your private key from your wallet
